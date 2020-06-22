81bb6e71c1655a0e5b748c3211a011cd8d245247
Quang Hung <ngochung.quang.1310@gmail.com>
2020-06-22 22:03:52 +0700

    Refactor

--- app.js
+++ app.js
@@ -1,30 +1,29 @@
-const express= require("express");
-const app= express();
+const express = require("express");
+const app = express();
 const session = require('express-session');
-const create = require('./modules/create');
-const path= require('path');
+const path = require('path');
 const pageRouter = require('./routes/pages');
 
-app.use(express.urlencoded({ extended: false}));
+app.use(express.urlencoded({extended: false}));
 
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 
-app.set('views',path.join(__dirname,'views'));
-app.set('view engine','pug');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 
 app.use(session({
-	secret:'Login',
-	resave : false,
-	saveUninitialized: false,
-	cookie: {
-		maxAge: 60 * 1000*30
-	}
+    secret: 'Login',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 60 * 1000 * 30
+    }
 }));
 
-app.use('/',pageRouter);
+app.use('/', pageRouter);
 
-app.listen(3000,()=> {
-	console.log('server is running on port 3000');
+app.listen(3000, () => {
+    console.log('server is running on port 3000');
 });
-module.exports= app;
+module.exports = app;
--- routes/pages.js
+++ routes/pages.js
@@ -1,60 +1,69 @@
 const express = require('express');
-const User = require('../core/user');
+const nodeFetch = require('node-fetch');
 const router = express.Router();
 
-const user= new User();
-
-router.get('/',(req,res)=>{
-    res.render('index',{
+router.get('/', (req, res) => {
+    res.render('index', {
         title: "HomePage"
     });
 });
 
-router.get('/register',(req,res,next)=>{
-   res.render('register-form'),{
-       title: "Register Page"
-   }
-})
+router.get('/register', (req, res, next) => {
+    res.render('register-form'), {
+        title: "Register Page"
+    }
+});
 
-router.get("/home",(req,res,next)=>{
+router.get("/home", (req, res, next) => {
+    res.render('home', {});
     let user = req.session.user;
-    if(user){
-        res.render('home',{opp:req.session.opp,name:user.email});
+    if (user) {
+        res.render('home', {opp: req.session.opp, name: user.email});
         return;
     }
     res.redirect('/');
 });
 
-router.post('/login',(req,res,next)=>{
-    //res.json(req.body);
-    user.login(req.body.email,req.body.password,function(result){
-        if(result){
- 
-             req.session.user = result;
-             req.session.opp=1;
-             res.redirect('/home');
-             //res.send('Welcomee '+result.email);
-        }else{
-            res.send('Username or password incorrect');
-        }
+router.post('/login', (request, response) => {
+    var username = null;
+    console.log(request.body);
+    nodeFetch.default('http://127.0.0.1:8080/e-voting/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            requestId: Date.now().toString(),
+            walletId: request.body.email,
+            password: request.body.password,
+        }),
     })
+        .then(res => {
+            if (res.ok) {
+                username = res.body();
+            }
+        })
+        .catch(err => {
+            response.json('err');
+        });
 
- 
+    response.render('home');
 });
-router.post('/register/post',(req,res,next)=>{
-    let userInput ={
+
+router.post('/register/post', (req, res, next) => {
+    let userInput = {
         email: req.body.email,
-        firstname:req.body.firstName,
+        firstname: req.body.firstName,
         lastname: req.body.lastName,
         password: req.body.password,
         type: req.body.type,
         sex: req.body.sex
     };
 
-    user.create(userInput,function(lastId){
-        if(lastId){
-            res.send('welcome '+ userInput.email);
+    user.create(userInput, function (lastId) {
+        if (lastId) {
+            res.send('welcome ' + userInput.email);
         }
-});
+    });
 });
 module.exports = router;
