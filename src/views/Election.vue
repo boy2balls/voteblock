0eec727ccba8c143ba323289bdb10569571c5c9a
Quang Hung <ngochung.quang.1310@gmail.com>
2020-09-01 01:09:34 +0700

    Update election

--- src/views/Election.vue
+++ src/views/Election.vue
@@ -2,11 +2,6 @@
   <div class="content">
     <div class="md-layout md-alignment-center">
       <div
-        class="md-layout-item md-medium-size-100 md-xsmall-size-100 md-size-66"
-      >
-        <election-form data-background-color="blue"></election-form>
-      </div>
-      <div
         class="md-layout-item md-medium-size-100 md-xsmall-size-100 md-size-100"
       >
         <md-card>
@@ -17,6 +12,11 @@
               <election-table table-header-color="green"></election-table>
           </md-card-content>
         </md-card>
+      </div>
+      <div
+        class="md-layout-item md-medium-size-100 md-xsmall-size-100 md-size-66"
+      >
+        <election-form data-background-color="blue"></election-form>
       </div>
     </div>
   </div>
